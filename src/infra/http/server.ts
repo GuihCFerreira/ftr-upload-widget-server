2d4959e05951dc3df1379b243d715a0bb4568758
Guilherme de Carvalho Ferreira <guihhh231@gmail.com>
2026-03-19 21:59:45 -0300

    chore: add swagger setup

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,8 +1,11 @@
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
-
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
@@ -31,6 +34,22 @@ server.setErrorHandler((error, _request, reply) => {
 })
 
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Widget Server',
+      description: 'API for handling image uploads',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.register(uploadImageRoute)
 
